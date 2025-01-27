a720f0b3c20a70a2b6ad54325b388d201699cb95
aburgy-93 <aburgy@uwalumni.com>
2025-01-27 13:46:46 -0600

    Webpack update

--- webpack.common.js
+++ webpack.common.js
@@ -2,36 +2,31 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true, // Cleans the output directory before each build
     },
-    devtool: "eval-source-map", // Use eval-source-map in development for faster rebuilds
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html", // Template file for generating the HTML
-        }),
-    ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: ["style-loader", "css-loader"], // For handling CSS files
             },
             {
                 test: /\.html$/i,
-                loader: "html-loader",
+                loader: "html-loader", // For processing HTML templates
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource", // Handles images as separate files
+                type: "asset/resource", // For handling images as separate assets
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html", // Template for the output HTML file
+        }),
+    ],
 };
